063597c2d25f82ef2ae522253e3bd260c30c920e
Nick Wong <njwon4@gmail.com>
2023-07-20 11:18:20 +1000

    fix: remove unused tabs

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -6,10 +6,4 @@
 
 <script lang="ts" setup>
 import AppLayout from '@/components/layouts/AppLayout.vue'
-
-const tabs = [
-  { name: 'Characters', href: '/characters', current: false },
-  { name: 'Species', href: '/species', current: false },
-  { name: 'Genders', href: '/genders', current: false },
-]
 </script>
